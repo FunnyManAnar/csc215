51e33d8ff417c81c2bc56b83e0d2d51d83fa3ce5
FunnyManAnar <hahappy_anar@yahoo.com>
2024-12-10 09:05:55 -0500

    .

--- Day_of_year.c
+++ Day_of_year.c
@@ -1,9 +1,9 @@
-int day_tab[2]*[13] ={
+int day_tab*[13] ={
     {0, 31, 29, 31, 30, 31,3 0, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
-day_of_year(year, month, day);
+day_of_year(year, month, day)
 int year, month, day;
 {
     int i, leap;
@@ -17,5 +17,11 @@ int year, month, day;
 month_day(year, yearday, pmonth, pday)
 int year, yearday, *pmonth, *pday;
 {
+    int 1, leap;
 
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for  (i = 1; yearday > day_tab[leap][i]; i++)
+        yearday -= day_tab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
 }
